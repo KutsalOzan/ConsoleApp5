42f6040b1555093f40296ddca7b66de67a58ab56
ozan kutsal <kutsalo@hotmail.com>
2021-05-23 03:08:02 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleApp5
+{
+    public class Motor
+    {
+
+
+
+        public Motor()
+        {
+            Renk = Color.Bisque;
+            VitesSayisi = 5;
+            YakitCinsi = "benzin";
+        }
+
+
+        public Color Renk { get; set; }
+        public byte VitesSayisi { get; set; }
+        public string YakitCinsi { get; set; }
+
+        public void Git() => Console.WriteLine("Gidiyorum");
+        public void Dur() => Console.WriteLine("Durdum");
+        public void VitesDegis() => Console.WriteLine("Vites Değiştir");
+
+        public void FarYak() => Console.WriteLine("Farları Yak");
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motor motor = new Motor();
+            Console.WriteLine("Değer atanmadığı için varsalıyan");
+            Console.WriteLine("Değerler Görüntülendi");
+
+            Console.WriteLine("Renk Özelliği: " + motor.Renk);
+            Console.WriteLine("Vites Sayısı Özelliği: " + motor.VitesSayisi);
+            Console.WriteLine("Yakıt Cinsi Özelliği: " + motor.YakitCinsi);
+            Console.WriteLine("------------------------------------------------");
+            Console.Write("Özelliklere değer atandıktan sonra: ");
+            motor.Renk = Color.White;
+            Console.WriteLine("Renk Özelliği: " + motor.Renk);
+            motor.VitesSayisi = 10;
+            Console.WriteLine("Vites Sayısı Özelliği " + motor.VitesSayisi);
+            motor.YakitCinsi = "Elektrikli Motor";
+            Console.WriteLine("Yakıt Cinsi Özelliği " +  motor.YakitCinsi);
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Metotlar");
+            motor.Git();
+            motor.Dur();
+            motor.VitesDegis();
+            motor.FarYak();
+            Console.ReadLine();
+        }
+    }
+}
